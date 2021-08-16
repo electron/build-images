c1eda51b40a46a81e061ef53b065d019933bdf47
Samuel Attard <samuel.r.attard@gmail.com>
2021-08-15 17:24:17 -0700

    build: add libnss3:i386 dependency

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -32,6 +32,7 @@ package_list_32bit="
     libgdk-pixbuf2.0-0:i386 \
     libdbus-1-3:i386
     libgbm1:i386 \
+    libnss3:i386 \
     libcurl4:i386"
 
 DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends $package_list
