e6bebd08a51a0d78ec23e5b3fd7e7c0846412328
John Kleinschmidt <jkleinsc@electronjs.org>
2022-01-24 10:14:44 -0500

    build: use ubuntu 20.04 (#19)

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -13,10 +13,9 @@ package_list="
     locales \
     lsb-release \
     nano \
+    python2 \
     python-dbus \
-    python-pip \
     python-setuptools \
-    python3.7 \
     python3-pip \
     sudo \
     vim-nox \
@@ -45,6 +44,8 @@ fi
 
 add-apt-repository ppa:git-core/ppa -y && apt-get update
 curl https://chromium.googlesource.com/chromium/src/+/HEAD/build/install-build-deps.sh\?format\=TEXT | base64 --decode | cat > /setup/install-build-deps.sh
+# Remove snapcraft to avoid issues on docker build
+sed -i 's/${dev_list} snapcraft/${dev_list}/g' /setup/install-build-deps.sh
 chmod +x /setup/install-build-deps.sh
 if [[ "$1" == "--multiarch" ]]; then
   bash /setup/install-build-deps.sh --syms --no-prompt --no-chromeos-fonts --lib32 --arm --no-nacl
@@ -63,18 +64,16 @@ DEBIAN_FRONTEND=noninteractive apt-get install -y --no-install-recommends nodejs
 rm -rf /var/lib/apt/lists/*
 npm i -g npm@latest
 
+# Install pip for python2 usage
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py --output get-pip.py
+python2 get-pip.py
+
 # crcmod is required by gsutil, which is used for filling the gclient git cache
 pip install wheel
 pip install -U crcmod
 
 # TODO: We can remove this step once transition to using python3 to run Electron tests is complete.
 pip install python-dbusmock==0.20.0
-
-# Set python3.7 as default python3
-update-alternatives  --install /usr/bin/python3 python3 /usr/bin/python3.7 10
-cd /usr/lib/python3/dist-packages
-cp apt_pkg.cpython-36m-x86_64-linux-gnu.so apt_pkg.so
-ln -s /usr/lib/python3/dist-packages/gi/_gi.cpython-{36m,37m}-x86_64-linux-gnu.so
 
 # dbusmock is needed for Electron tests
 pip3 install wheel
