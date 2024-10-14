805399912c471214d48b6200ff0026c45317ee8a
Shelley Vohr <shelley.vohr@gmail.com>
2024-10-14 18:46:15 +0200

    refactor: use nvm for Node.js installation

--- tools/install-deps.sh
+++ tools/install-deps.sh
@@ -102,12 +102,10 @@ fi
 rm -rf /var/lib/apt/lists/*
 
 # Install Node.js
-mkdir -p /etc/apt/keyrings
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
-apt-get update
-apt-get install nodejs -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+nvm install 20
 
 # Install Yarn
 npm i -g yarn
